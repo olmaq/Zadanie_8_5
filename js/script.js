201bb24a3c9e637aa38b73d7c32330011fb95004
Krzysztof Olma <olmak3910@gmail.com>
2018-01-06 14:50:19 +0100

    Exercise_8_5

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+// for console
+var a = 10;
+var h = 5,
+triangleArea = a*h/2;
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+
+// with prompter
+var a = prompt('Enter triangle base a:');
+var h = prompt('Enter triangle height h:')
+triangleArea = a*h/2;
+alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
